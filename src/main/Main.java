b58e723decaae72d6e2879575bb5c937c2ebeb06
Mac Copley <qcopley@gmail.com>
2015-09-27 03:59:05 +1300

    added a println

--- src/main/Main.java
+++ src/main/Main.java
@@ -20,6 +20,7 @@ import graphics.GameCanvas;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("running this shit");
 
 		System.out
 				.println("welcome to the program. There should be an initialisation dialog that is shown now that\n lets you create a server and then create a new player and join that server separately\n we will pretend that is done automagically for now so that i dont have to create those dialogs");
