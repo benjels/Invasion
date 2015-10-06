5e245557539bc48af37b0f8b5afbec98aa503456
josh-gomez72 <josh.gomez72@gmail.com>
2015-10-06 18:18:26 +1300

    Loading now has a visual representation of which event is which and prints them to a file. Just used to see what is happening and set up the structure of the parser

--- src/storage/XMLParser.java
+++ src/storage/XMLParser.java
@@ -4,9 +4,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 
 import javax.xml.stream.FactoryConfigurationError;
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
@@ -17,33 +19,68 @@ public class XMLParser {
 	public WorldGameState parse(File file) {
 		try {
 			InputStream in = new FileInputStream(file);
-			//InputStream in = new FileInputStream(new File("test.xml"));
-			XMLStreamReader xmlreader = XMLInputFactory.newInstance().createXMLStreamReader(in);
-			
-			while(xmlreader.hasNext()){
-				//fill this in with each individual case if it comes to a worldstate tag, tile tag, room tag, entity tag etc
-				
+			// InputStream in = new FileInputStream(new File("test.xml"));
+			PrintWriter out = new PrintWriter(new File("loadingTest.txt"));
+			XMLStreamReader xmlreader = XMLInputFactory.newInstance()
+					.createXMLStreamReader(in);
+
+			while (xmlreader.hasNext()) {
+				// fill this in with each individual case if it comes to a
+				// worldstate tag, tile tag, room tag, entity tag etc
+
 				int event = xmlreader.next();
-				
-				//Shows that it is a start tag
-				if (event == xmlreader.START_ELEMENT){
-					try {
-					String text = xmlreader.getElementText();
-					String tagName = xmlreader.getLocalName();
-					System.out.println("Local Name: " + tagName);
-					System.out.println("Text: " + text);
-					} catch(XMLStreamException e){
-						//
+				/*
+				 * Look at XMLStreamConstants.class if needing to add in other
+				 * values
+				 */
+				switch (event) {
+				case 1:
+					// event is start element
+					out.println("Start: <" + xmlreader.getLocalName() + ">");
+					break;
+				case 2:
+					// event is an end element
+					out.println("End: </" + xmlreader.getLocalName() + ">");
+
+					break;
+				case 4:
+					// event is characters /*not sure if i need to use */
+					if (xmlreader.hasText() && xmlreader.getText() != "\t") {
+						
+						out.println("@" + xmlreader.getText());
 					}
+					// out.println(xmlreader.getElementText());
+					break;
+				case 5:
+					// event is a comment /*will probably just ignore these
+					// aswell*/
+					break;
+				case 6:
+					// event is ignorable whitespace
+					out.print("WHITESPACE");
+					break;
+				case 7:
+					// event is start of document
+					out.println("Start of document");
+					break;
+				case 8:
+					// event is end of document
+					out.println("End of document");
+					break;
+				case 10:
+					// event is an attribute
+					break;
+				case 11:
+					// event is a DTD
+					out.println("DTD");
+					break;
+				case 13:
+					// event is NameSpace
+					out.println("NameSpace");
+					break;
+				default:
 				}
-				
-				if (event == xmlreader.CHARACTERS){
-					System.out.println("type is characters");
-				}
-				
-				
-				
-				
+
 			}
 
 		} catch (FileNotFoundException | XMLStreamException
