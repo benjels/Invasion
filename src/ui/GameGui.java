5cb160fb0e4e49cda8ef36d6ac2053356b4d4d54
Mac Copley <qcopley@gmail.com>
2015-10-13 21:10:54 +1300

    added save button

--- src/ui/GameGui.java
+++ src/ui/GameGui.java
@@ -18,121 +18,124 @@ import control.Listener.MyMouseAction;
 
 public class GameGui{
 
-	private static final long serialVersionUID = 1L;
-	private JFrame frame;
-	private JPanel contentPane;
-	private final GameCanvas invasionCanvas;
-	private JPanel lowerPanel;
-	private PlayerCanvas playerCanvas;
-	private JMenuBar menuBar;
-	private JMenu menu;
-	private JMenuItem mntmStatgame;
-	private JMenu mnGame;
-	private JMenuItem mntmExit;
+    private static final long serialVersionUID = 1L;
+    private JFrame frame;
+    private JPanel contentPane;
+    private final GameCanvas invasionCanvas;
+    private JPanel lowerPanel;
+    private PlayerCanvas playerCanvas;
+    private JMenuBar menuBar;
+    private JMenu menu;
+    private JMenuItem mntmStatgame;
+    private JMenuItem mntmSavegame;
+    private JMenu mnGame;
+    private JMenuItem mntmExit;
 
-	public GameGui(GameCanvas invasionGameCanvas){ //package protected
-		this.invasionCanvas = invasionGameCanvas;
-		this.initialize();
-	}
+    public GameGui(GameCanvas invasionGameCanvas){ //package protected
+        this.invasionCanvas = invasionGameCanvas;
+        this.initialize();
+    }
 
-	private void initialize() {
-		frame = new JFrame("Invasion");
-		frame.getContentPane().setLayout(new BorderLayout(0, 0));
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setBounds(0, 0, 1500,1120);
+    private void initialize() {
+        frame = new JFrame("Invasion");
+        frame.getContentPane().setLayout(new BorderLayout(0, 0));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(0, 0, 1500,1120);
 
-		contentPane = new JPanel();
-		contentPane.setBackground(Color.PINK);
-		frame.setContentPane(contentPane);
-		contentPane.setLayout(null);
+        contentPane = new JPanel();
+        contentPane.setBackground(Color.PINK);
+        frame.setContentPane(contentPane);
+        contentPane.setLayout(null);
 //		invasionCanvas.setBackground(new Color(0, 255, 255)); // original
 //		invasionCanvas.setBackground(new Color(242,236,247)); //next original
-		invasionCanvas.setBackground(new Color(14,34,0));
-		invasionCanvas.setBounds(0, 20, 1500, 800);
-		contentPane.add(invasionCanvas);
+        invasionCanvas.setBackground(new Color(14,34,0));
+        invasionCanvas.setBounds(0, 20, 1500, 800);
+        contentPane.add(invasionCanvas);
 
-		lowerPanel = new JPanel();
-		lowerPanel.setBackground(new Color(14,34,0));
-		lowerPanel.setBounds(0, 820, 1500, 200);
-		contentPane.add(lowerPanel);
-		lowerPanel.setLayout(null);
+        lowerPanel = new JPanel();
+        lowerPanel.setBackground(new Color(14,34,0));
+        lowerPanel.setBounds(0, 820, 1500, 200);
+        contentPane.add(lowerPanel);
+        lowerPanel.setLayout(null);
 
-		playerCanvas = new PlayerCanvas();//rgb(192,192,192)
-		playerCanvas.setBackground(new Color(14,34,0));
-		playerCanvas.setBounds(0, 0, 1500, 200);
-		lowerPanel.add(playerCanvas);
+        playerCanvas = new PlayerCanvas();//rgb(192,192,192)
+        playerCanvas.setBackground(new Color(14,34,0));
+        playerCanvas.setBounds(0, 0, 1500, 200);
+        lowerPanel.add(playerCanvas);
 
-		menuBar = new JMenuBar();
-		menuBar.setBounds(0, 0, 1500, 20);
-		contentPane.add(menuBar);
+        menuBar = new JMenuBar();
+        menuBar.setBounds(0, 0, 1500, 20);
+        contentPane.add(menuBar);
 
-		menu = new JMenu("File");
-		menuBar.add(menu);
+        menu = new JMenu("File");
+        menuBar.add(menu);
 
-		mntmStatgame = new JMenuItem("Start Game");
-		menu.add(mntmStatgame);
+        mntmStatgame = new JMenuItem("Start Game");
+        menu.add(mntmStatgame);
+        mntmSavegame = new JMenuItem("Save Game");
+        menu.add(mntmSavegame);
 
-		mnGame = new JMenu("Game");
-		menuBar.add(mnGame);
+        mnGame = new JMenu("Game");
+        menuBar.add(mnGame);
 
-		mntmExit = new JMenuItem("Exit");
-		mnGame.add(mntmExit);
+        mntmExit = new JMenuItem("Exit");
+        mnGame.add(mntmExit);
 
-		frame.setVisible(true);
-		//frame.setResizable(false);
+        frame.setVisible(true);
+        //frame.setResizable(false);
 
-	}
+    }
 
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
 
-	public JPanel getContentPane() {
-		return contentPane;
-	}
+    public JPanel getContentPane() {
+        return contentPane;
+    }
 
-	public GameCanvas getInvasionCanvas() {
-		return invasionCanvas;
-	}
+    public GameCanvas getInvasionCanvas() {
+        return invasionCanvas;
+    }
 
-	public JPanel getLowerPanel() {
-		return lowerPanel;
-	}
+    public JPanel getLowerPanel() {
+        return lowerPanel;
+    }
 
-	public PlayerCanvas getPlayerCanvas() {
-		return playerCanvas;
-	}
+    public PlayerCanvas getPlayerCanvas() {
+        return playerCanvas;
+    }
 
-	public JMenuBar getJMenuBar() {
-		return menuBar;
-	}
+    public JMenuBar getJMenuBar() {
+        return menuBar;
+    }
 
-	public JMenu getMenu() {
-		return menu;
-	}
+    public JMenu getMenu() {
+        return menu;
+    }
 
-	public JMenuItem getMntmStatgame() {
-		return mntmStatgame;
-	}
+    public JMenuItem getMntmStatgame() {
+        return mntmStatgame;
+    }
 
-	public JMenu getMnGame() {
-		return mnGame;
-	}
+    public JMenu getMnGame() {
+        return mnGame;
+    }
 
-	public JMenuItem getMntmExit() {
-		return mntmExit;
-	}
+    public JMenuItem getMntmExit() {
+        return mntmExit;
+    }
 
-	public void initializeCanvasListeners(MyMouseAction maction,KeyListener saction) {
-		this.invasionCanvas.addMouseListener(maction);
-		this.invasionCanvas.addKeyListener(saction);
-		//this.playerCanvas.addKeyListener(saction);
-		this.playerCanvas.addMouseListener(maction);
-	}
+    public void initializeCanvasListeners(MyMouseAction maction,KeyListener saction) {
+        this.invasionCanvas.addMouseListener(maction);
+        this.invasionCanvas.addKeyListener(saction);
+        //this.playerCanvas.addKeyListener(saction);
+        this.playerCanvas.addMouseListener(maction);
+    }
 
-	public void initializeMenuListeners(ActionListener ma) {
-		mntmExit.addActionListener(ma);
-		mntmStatgame.addActionListener(ma);
-	}
+    public void initializeMenuListeners(ActionListener ma) {
+        mntmExit.addActionListener(ma);
+        mntmStatgame.addActionListener(ma);
+    }
 
 }
