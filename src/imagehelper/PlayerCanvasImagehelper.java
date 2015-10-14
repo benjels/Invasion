5c609c08839615b1f5026a3fe3fba6d1d3482cb2
Mac Copley <qcopley@gmail.com>
2015-10-14 15:24:05 +1300

    changed field names in Render Classes. Prints 3 missing image item descriptions now

--- src/imagehelper/PlayerCanvasImagehelper.java
+++ src/imagehelper/PlayerCanvasImagehelper.java
@@ -35,6 +35,7 @@ public class PlayerCanvasImagehelper {
 		ImageIcon nvg = new ImageIcon("images/InventoryImages/nightVisionGoggles.jpg");
 		ImageIcon nullentity = new ImageIcon("images/InventoryImages/nullentity.png");
 		ImageIcon sbag = new ImageIcon("images/InventoryImages/smallCarrier.png");
+		ImageIcon sbaginv = new ImageIcon("images/InventoryImages/smallCarrierinv.png");
 		ImageIcon teleportGun = new ImageIcon("images/InventoryImages/teleportGun.png");
 		ImageIcon coin = new ImageIcon("images/InventoryImages/coin.png");
 
@@ -43,9 +44,15 @@ public class PlayerCanvasImagehelper {
 		ImageIcon keycardinv = new ImageIcon("images/InventoryImages/keycardinv.png");
 		ImageIcon nvginv = new ImageIcon("images/InventoryImages/nvginv.png");
 		ImageIcon nullentityinv = new ImageIcon("images/InventoryImages/nullentityinv.png");
-		ImageIcon mediumCarrier = new ImageIcon("images/InventoryImages/mediumCarrerinv.png");
-		ImageIcon teleportGuninv = new ImageIcon("images/InventoryImages/teleportGuninv.png");
 
+		ImageIcon mbaginv = new ImageIcon("images/InventoryImages/mediumCarrierinv.png");
+
+		ImageIcon teleportGuninv = new ImageIcon("images/InventoryImages/teleportGuninv.png");
+		ImageIcon healthKitinv = new ImageIcon("images/InventoryImages/healthKitinv.png");
+		ImageIcon healthKit = new ImageIcon("images/InventoryImages/healthKit.png");
+
+		getPlayerCanvasImages().put("healthkit", healthKit);
+		getPlayerCanvasImages().put("healthkitinv", healthKitinv);
 		// Player Inventory Items.
 		getPlayerCanvasImages().put("gun",gun);
 		getPlayerCanvasImages().put("keycard",keycard);
@@ -54,32 +61,33 @@ public class PlayerCanvasImagehelper {
 		getPlayerCanvasImages().put("lockedTeleport",lockedTeleport);
 		getPlayerCanvasImages().put("map",map);
 		getPlayerCanvasImages().put("mbag", mbag);
+
 		getPlayerCanvasImages().put("nvg",nvg);
 		getPlayerCanvasImages().put("nullentity", nullentity);
-		getPlayerCanvasImages().put("sbag", sbag);
+		getPlayerCanvasImages().put("smallCarrier", sbag);
+		getPlayerCanvasImages().put("smallCarrierinv", sbaginv);
 		getPlayerCanvasImages().put("teleportGun",teleportGun);
-
 
 		getPlayerCanvasImages().put("coininv", coininv);
 		getPlayerCanvasImages().put("guninv", guninv);
 		getPlayerCanvasImages().put("keycardinv", keycardinv);
 		getPlayerCanvasImages().put("nvginv", nvginv);
 		getPlayerCanvasImages().put("nullentityinv", nullentityinv);
-		getPlayerCanvasImages().put("mbaginv", mediumCarrier);
+		getPlayerCanvasImages().put("mbaginv", mbaginv);
 		getPlayerCanvasImages().put("teleportGuninv",teleportGuninv);
-
-
-
 
 		// player character images and buttons.
 		getPlayerCanvasImages().put("warriorIcon", warriorIcon);
 		getPlayerCanvasImages().put("priestIcon", priestIcon);
 		getPlayerCanvasImages().put("playerCanvasButtons", playerCanvasButtons);
-
-
 	}
 
 	/**
+	 *
+	 *
+	 *
+	 *
+	 *
 	 * player canvas image helper method to retrieve images.
 	 * @param filename
 	 * @return
